74d45c1dfb00c3e70a362253942c1eecb72faa7c
rl_zhu <rl_zhu@outlook.com>
2023-01-15 19:32:19 -0500

    still unable to readFile may consider to store as json data at backend

--- websites/src/components/files-excel/excel-local.js
+++ websites/src/components/files-excel/excel-local.js
@@ -1,19 +1,18 @@
 import React, { useState } from 'react'
-import excelfile from './testExcel.xlsx'
+// import * as fs from 'fs';
+import excelfile from './test.xlsx'
 import { read, utils, writeFile, readFile, XLSX } from 'xlsx';
 
 const ParseExcel = () => {
-    // const [fileContent, setFileContent] = useState(null);
-    // const fileData = readFile("./testExcel");
-    // console.log(fileData)
+  // const [fileContent, setFileContent] = useState(null);
+  // const fileData = readFile("./testExcel");
+  // console.log(fileData)
+  // const fs = require('fs-extra')
+  const XLSX = require('xlsx');
+  // const data = fs.readFileSync('./test.xlsx');
+  const workbook = read(data, { type: 'buffer' });
 
-    const filePath = './testExcel.xlsx';
-    const workbook = readFile(filePath);
-    const sheetName = workbook.SheetNames[0];
-    const sheet = workbook.Sheets[sheetName];
-    const jsonData = XLSX.utils.sheet_to_json(sheet);
-    console.log(jsonData);
-    
+
   return (
     <div>
       {/* <Document file={excelfile}></Document> */}
