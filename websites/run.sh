8742d9fe3b2816dc15f278ba208baf2edd522265
Chester Guan （Ziyuan Guan） <ziyuan.guan@ufl.edu>
2023-01-26 12:38:38 -0500

    Add both development and production dockerFiles

--- websites/run.sh
+++ websites/run.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-cd ic3-online-documentation/websites/
+## set up crontab job
+echo "0 1 * * *  su -s /bin/sh nobody -c 'cd /ic3-online-documentation && /usr/local/bin/git pull'" >> /etc/crontabs/root
+crond -l 2 -f > /dev/stdout 2> /dev/stderr &
+
+## start documentation
+cd ic3-online-documentation/websites
 
 ## install dependencies
 echo "Install Node Dependencies"
@@ -8,3 +13,4 @@ npm install .
 
 ## start development mode
 npm run start
+
