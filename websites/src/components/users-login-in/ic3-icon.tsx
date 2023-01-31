5453a03e7bf4b46fd20045a3a1dd7b4166849169
Chester Guan （Ziyuan Guan） <ziyuan.guan@ufl.edu>
2023-01-31 11:28:17 -0500

    try merge

--- websites/src/components/users-login-in/ic3-icon.tsx
+++ websites/src/components/users-login-in/ic3-icon.tsx
@@ -1,7 +1,7 @@
 import Image from 'react'
 import React from 'react'
-import Ufhealth from '../../../static/img/Ufhealth.png'
-import IC3Center from '../../../static/img/IC3-center.png'
+import Ufhealth from '@site/static/img/Ufhealth.png'
+import IC3Center from '@site/static/img/IC3-center.png'
 import styles from './index.module.css'
 
 const Ic3Icon = () => {
